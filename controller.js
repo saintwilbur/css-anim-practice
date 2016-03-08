b8c746e4088e872470c8ed01d097211d0fc0c80b
Mitch Rogers <sainwilbur@gmail.com>
2016-03-08 03:51:40 -0800

    color rings on mouseover, the ram destroyer

--- controller.js
+++ controller.js
@@ -2,17 +2,18 @@ var el = document.getElementById('bodyarea');
 
 //source: kirupa
 //https://www.kirupa.com/html5/getting_mouse_click_position.htm
-el.addEventListener('click', getClickPosition, false);
+el.addEventListener('mouseover', getHoverPosition, false);
 
-function getClickPosition(e) {
+function getHoverPosition(e) {
 	var parentPosition = getPosition(e.currentTarget);
 	var xPosition = e.clientX - parentPosition.x;
 	var yPosition = e.clientY - parentPosition.y;
 	console.log(xPosition, yPosition);
 	var div = document.createElement("div");
-	div.setAttribute('class', 'firefly');
-	div.style.top = yPosition;
-	div.style.left = xPosition;
+	//div.setAttribute('class', 'square-circle');
+    div.setAttribute('class', 'firefly');
+	div.style.top = yPosition - 2;
+	div.style.left = xPosition - 2;
 	document.body.appendChild(div);	
 }
 
